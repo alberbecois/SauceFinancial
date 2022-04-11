dadb7710c7132af3ad8e5dd5d6a5d5f1c141b1af
Joshua Knutson <37628377+alberbecois@users.noreply.github.com>
2022-04-11 18:07:37 -0400

    Add Google Fonts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <section class="my-4">
+        <h1>Let's get this gravy</h1>
+        <p>Banking just got better with rewards that you earn on money you were spending anyway.</p>
+    </section>
     
 @endsection
